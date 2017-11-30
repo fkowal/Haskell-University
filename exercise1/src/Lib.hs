f29c636339d93bc7d420ed9514d59361e87ca11a
Maciej Kowalski <maciej.kowalski@allegrogroup.com>
2017-11-30 20:13:45 +0100

    control.monad forever loop

--- exercise1/src/Lib.hs
+++ exercise1/src/Lib.hs
@@ -1,8 +1,11 @@
 module Lib
     (
     greetUser,
-    greetLoop
+    greetLoop,
+    greetLoop2
     ) where
+
+import Control.Monad
 
 greet :: IO ()
 greet = putStrLn "Hello, what is your name?"
@@ -18,5 +21,8 @@ greetUser = greet >> (getName >>= sayHi)
 
 greetLoop :: IO ()
 greetLoop = greetUser >> greetLoop
+
+greetLoop2 :: IO ()
+greetLoop2 = forever greetUser
 -- greetUser =
 --         putStrLn "Hello, what is your name?" >> (readLn >>= (\name -> putStrLn ("Nice to meet you " ++ name)))
